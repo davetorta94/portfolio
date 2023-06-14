95686e5860ddf4288712867a356ab045082a4fb5
David <david.torta.olah@gmail.com>
2023-06-14 07:43:55 +0200

    english page corrected

--- src/HomeEng.jsx
+++ src/HomeEng.jsx
@@ -5,25 +5,26 @@ import profilepic from './images/fotoperfil.jpg'
 export const HomeEng = () => {
     return (
       <>
-      <div className="container-fluid home mt-5" id='home'>
-          <div className="row black text-white animate__animated animate__fadeIn">
-            <div className="col-xl-6 col-lg-12 d-flex flex-column text-center justify-content-center">
+      <div className="container-fluid home mt-lg-5 mt-md-5" id='home'>
+        <div className="row black text-white">
+        <div className="col-xl-6 col-lg-12 mt-sm-5 mt-md-5 d-flex flex-column text-center justify-content-center">
               <h2>Hi! My name is David Torta</h2>
               <p>and I am a frontend developer specialized in React</p>
             </div>
-            <div className="col-xl-4 col-lg-12">
-               <div className="card bg-dark sombrita">
+            <div className="col-xl-4 col-lg-12 col-md-12 d-md-flex justify-content-md-center">
+               <div className="card fondo-card sombrita">
                  <img src={profilepic} className='profilepic' alt="foto"></img>
-                 <div className="card-body bg-dark text-white border-top-1">
+                 <div className="card-body fondo-card text-white border-top-1">
                        <p className="card-text">David Torta Olah</p>
                      </div>
                  </div>
                </div>
-            
-          </div>
         </div>
-      
+      </div>
+
   
       </>
     )
   }
+
+ 
