f83ac31eee87c8fab88c9affcd328e5bc8a7e4cf
David <david.torta.olah@gmail.com>
2024-02-20 18:48:23 +0100

    portfolio css good

--- src/Proyectos.jsx
+++ src/Proyectos.jsx
@@ -1,6 +1,7 @@
 import { Parallax } from 'react-scroll-parallax';
 import todopic from './images/captura-todo.png'
 import playerpic from './images/captura-reproductor.png'
+import myfpic from './images/captura-myf.png'
 
 export const Proyectos = () => {
 
@@ -16,15 +17,22 @@ export const Proyectos = () => {
       </div>
       <div className="row project-images mx-xl-5 pb-5 mx-sm-4 mx-md-4">
 
+            <div className="col-lg-12 text-white text-center mx-lg-5 mx-md-0 mt-md-2 mt-sm-2">
+          <Parallax scale={[1,1.75]}>
+                <h4 className='pb-2'>Web Agencia</h4>
+                <a href="https://65cbb1260aab9905f227bee6--aesthetic-faloodeh-62b027.netlify.app/" target='_blank'><img src={myfpic} alt="" className='linkpic' ></img></a>
+          </Parallax>
+            </div>
+
             <div className="col-lg-4 col-md-12 text-white text-center mx-lg-5 mx-md-0 mt-md-2 mt-sm-2">
-          <Parallax scale={[1,2]}>
+          <Parallax scale={[1,1.5]}>
                 <h4 className='pb-2'>App de Tareas</h4>
                 <a href="https://sparkling-panda-572dfa.netlify.app/?" target='_blank'><img src={todopic} alt="" className='linkpic' ></img></a>
           </Parallax>
             </div>
           
           <div className="col-lg-4 col-md-12 text-white text-center mx-md-5 mt-md-2 second-project-pic">
-              <Parallax scale={[1,2]}>
+              <Parallax scale={[1,1.5]}>
               <h4 className='pb-2'>Reproductor de Música</h4>
               <a href="https://chipper-sorbet-86ac03.netlify.app/" target='_blank'><img src={playerpic} alt="" className='linkpic' ></img></a>
               </Parallax>
