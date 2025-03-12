dce885176aa22a1ae6a679e4e427e82cb6673bdd
David <david.torta.olah@gmail.com>
2025-03-12 12:45:10 +0100

    update

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import emailjs from 'emailjs-com';
 
 export const ContactForm = ({handleSubmit, onInputChange, name, email, message, handleLanguage, spanish}) => {
   return (
--- src/pages/DetourPage.jsx
+++ src/pages/DetourPage.jsx
@@ -19,9 +19,16 @@ export const DetourPage = ({spanish, handleLanguage}) => {
                      </div>
 
                      <div className="row bg-about text-white d-flex justify-content-around mt-2 mb-5 pt-2 sombrita animate__animated animate__slideInDown">
-                       <div className="col-lg-4 col-md-8 col-sm-10 pt-2 about-text"><p>Por decisión personal de la clienta, ésta página web permanecerá en modo mantenimiento por un tiempo limitado. Sin embargo ha concedido permiso para desactivar este modo y mostrar así su contenido bajo un periodo límitado y bajo mi supervisión.</p>
+                       <div className="col-lg-4 col-md-8 col-sm-10 pt-2 about-text"><p>Por decisión personal de la clienta, ésta página web permanecerá en modo mantenimiento por un tiempo limitado. Sin embargo ha concedido permiso para desactivar este modo y mostrar así su contenido de forma momentánea y bajo mi supervisión.</p>
                        </div>
                      </div>
+
+                     <div className='row text-white d-flex justify-content-center animate__animated animate__slideInDown'>
+                        <p className='text-white px-1'>PÁGINA WEB: </p> 
+                        <button className='d-flex justify-content-around bg-dark'>
+                         <a href="https://amandaweslyn.com/" target='_blank'> https://amandaweslyn.com/</a>
+                        </button>
+                      </div>
                  </div>
             :
                         <div className="container-fluid margin-per mx-xs-0">
@@ -30,9 +37,16 @@ export const DetourPage = ({spanish, handleLanguage}) => {
                     </div>
 
                     <div className="row bg-about text-white d-flex justify-content-around mt-2 mb-5 pt-2 sombrita animate__animated animate__slideInDown">
-                      <div className="col-lg-4 col-md-8 col-sm-10 pt-2 about-text"><p>Due to the client's personal decision, this website will remain in maintenance mode for a limited time. However, she has granted permission to temporarily disable this mode and display its content for a limited period under my supervision.</p>
+                      <div className="col-lg-4 col-md-8 col-sm-10 pt-2 about-text"><p>Due to the client's personal decision, this website will remain in maintenance mode for a limited time. However, she has granted permission to temporarily disable this mode and display its content for a short period under my supervision.</p>
                       </div>
                     </div>
+
+                      <div className='row text-white d-flex justify-content-center animate__animated animate__slideInDown'>
+                      <p className='text-white px-1'>WEBSITE:</p>
+                        <button className='d-flex justify-content-around bg-dark'>
+                         <a href="https://amandaweslyn.com/" target='_blank'> https://amandaweslyn.com/</a>
+                        </button>
+                      </div>
                 </div>
         }
         
